c2b4e27c3ef04dff1e89d0d165c5fc417e4ebda1
Christoph Holthaus <christoph.holthaus+git@choltha.net>
2023-05-12 00:10:46 +0000

    dev

--- addEmbeddings.py
+++ addEmbeddings.py
@@ -80,11 +80,3 @@ sorted_df_final = sorted_df.copy()
 #df_test['ada_embedding'] = df_test['Content'].apply(lambda x: get_embedding(x, engine='text-embedding-ada-002'))
 sorted_df_final['ada_embedding'] = sorted_df.Content.apply(lambda x: get_embedding(x, engine='text-embedding-ada-002'))
 sorted_df_final.to_csv("./data/structured-content-ada-002.csv")
-
-# Get embeddings and save them for future reuse
-# Ensure you have your API key set in your environment per the README: https://github.com/openai/openai-python#usage
-'''
-# This may take a few minutes
-df["embedding"] = df.Content.apply(lambda x: get_embedding(x, engine=embedding_model))
-df.to_csv("data/fine_food_reviews_with_embeddings_1k.csv")
-'''
