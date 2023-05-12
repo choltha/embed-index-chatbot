1f091ad999414e91ef02b463cf9183404f2da52c
Christoph Holthaus <christoph.holthaus+git@choltha.net>
2023-05-12 00:28:07 +0000

    fixme

--- ssearch.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import numpy as np
-
-datafile_path = "./data/structured-content-ada.csv"
-df = pd.read_csv(datafile_path)
-df["ada_embedding"] = df.ada_embedding.apply(eval).apply(np.array)
