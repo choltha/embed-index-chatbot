be2945296e057cf3f5b65b5d05648a43ba69053c
Christoph Holthaus <christoph.holthaus+git@choltha.net>
2023-05-12 11:26:00 +0000

    dev

--- chatbot.py
+++ chatbot.py
@@ -3,21 +3,23 @@ import search
 import sys
 import pandas as pd
 import yaml
+import openai
 
 # Assuming you want to read the first command-line argument
 template_config = sys.argv[1]
 user_query = sys.argv[2]
 
-print("template_config:", config)
+print("template_config:", template_config)
 print("user_query:", user_query)
+#print(openai.api_key)
 
-total_tokens = int(settings.GPT_MAX_TOKENS)
+total_tokens = int(settings.GPT_MAX_TOKENS_FULL_PROMPT)
 answer_token_placeholder = 500
 fewshot_guidance_token = 1000
 token_for_content = total_tokens - answer_token_placeholder - fewshot_guidance_token
 #print (token_for_content) # should be 2596
 
-df_top_10=search.get_top_10_similar(param)
+df_top_10=search.get_top_10_similar(user_query)
 print(df_top_10)
 
 concatenated_text = ""
@@ -41,7 +43,7 @@ print ("Total Tokens Added:", total_tokens_added)
 print("Concatenated Text:", concatenated_text)
 
 # import promt template
-with open('./prompt-templates/', 'r') as file:
+with open('./prompt-templates/manual.txt', 'r') as file:
     template = file.read()
 
 # import template config
@@ -50,7 +52,6 @@ with open(template_config, 'r') as file:
 
 # data which is not in the template config
 additional_data = {
-    'user_query': user_query,
     'reference_data': concatenated_text
 }
 # Combine the data from the YAML file and the additional data into a single dictionary:
@@ -59,8 +60,22 @@ data = {**template_data, **additional_data}
 # fill in the placeholders
 completed_template = template.format(**data)
 print(completed_template)
-
+print("##############")
 
 # run gpt query
+
+messages=[ 
+     {"role": "system", "content": completed_template},
+     {"role": "user", "content": user_query}
+     ]
+
+print(openai.ChatCompletion.create(
+    model=settings.GPT_MODEL,
+    messages=messages,
+    max_tokens=200, 
+    temperature = 0.7,
+    stream = False
+    ))
+
 # show results
 
